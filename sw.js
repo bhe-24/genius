0b217bdc4ad18fd52576038e5281c819855c2b41
Mukh. Bayu Aji Tolafudin <ragamliterasi@gmail.com>
2026-08-25 10:37:40 +0700

    Implement notification click handling in service worker
    Add notification click handling to open specific URLs.

--- sw.js
+++ sw.js
@@ -24,6 +24,31 @@ self.addEventListener('activate', event => {
 
 // Strategi: Network First (Utamakan internet, jika gagal baru ambil cache)
 self.addEventListener('fetch', event => {
+
+  // Menangani aksi ketika Notifikasi diklik oleh pengguna
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close(); // Tutup pop-up notifikasi
+    
+    // Ambil URL tugas spesifik dari payload data notifikasi
+    const urlToOpen = event.notification.data.url;
+
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+            // Jika aplikasi sudah terbuka, fokuskan dan arahkan ke URL tugas
+            for (let i = 0; i < windowClients.length; i++) {
+                const client = windowClients[i];
+                if (client.url && 'focus' in client) {
+                    client.navigate(urlToOpen);
+                    return client.focus();
+                }
+            }
+            // Jika aplikasi sedang ditutup, buka aplikasi baru ke URL tugas
+            if (clients.openWindow) {
+                return clients.openWindow(urlToOpen);
+            }
+        })
+    );
+});
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
